3c3c846b27f0704baddef818fa9028a3a41a01a2
pitchingcoachu <jgaynor@pitchingcoachu.com>
2026-01-10 22:42:19 -0700

    Update school_config.R

--- config/school_config.R
+++ config/school_config.R
@@ -1,33 +1,22 @@
 # School-specific overrides for the shared app.
 # Copy this file to another repo and keep the same structure when you need to customize colors, logos, APIs, etc.
 school_config <- list(
-  team_code = "OSU",
+  team_code = "Harvard",
   # Player filters
   allowed_pitchers = c(
-    "Lee, Aidan",
-    "Limas, Jacob",
-    "Higginbottom, Elijah",
-    "Cunnings, Cam",
-    "Moeller, Luke",
-    "Smith, Jace",
-    "Frey, Chase",
-    "Ahern, Garrett",
-    "McGuire, Tommy",
-    "Robb, Nicholas",
-    "Guerrero, JT",
-    "Gregory, Billy",
-    "Penzkover, Gunnar",
-    "Lewis, JT",
-    "Kiemele, Cody",
-    "Cohen, Andrew",
-    "Lyon, Andrew",
-    "Johns, Tanner",
-    "Toney, Brock",
-    "Sloan, Landon",
-    "Key, Chance",
-    "Orr, Dillon",
-    "Yates, Zach",
-    "New, Cody"
+  "Abler, Andrew",
+  "Alagheband, Luca",
+  "Burns, Will",
+  "Bergsma, Charley",
+  "Colasante, Gio",
+  "Dowling, Brian",
+  "Gable, Brett",
+  "Kahn, Davis",
+  "McHugh, Ryan",
+  "Sams, Daniel",
+  "Sharma, Vedant",
+  "Smith, Jack",
+  "Tahnk, Owen"
   ),
   allowed_hitters = c(
     "Wentworth, TP",
@@ -113,23 +102,19 @@ school_config <- list(
     background_secondary= "#e8ecf1"   # the matching secondary background tone
     
   ),
-  logo = "OSUlogo.png",
+  logo = "Harvardlogo.png",
   coaches_emails = c(
-    "Blake.hawksworth@okstate.edu",
-    "Payton.stevens@okstate.edu",
-    "Trey.cobb@okstate.edu",
-    "jared.s.gaynor@gmail.com",
-    "Victor.Romero@okstate.edu",
-    "J.Holliday@okstate.edu",
-    "Mark.Ginther@okstate.edu",
-    "hub.roberts@okstate.edu"
+    "nathancole@fas.harvard.edu",
+    "jeffrey_kane@fas.harvard.edu",
+    "mslattery@fas.harvard.edu",
+    "cleaden@seas.harvard.edu"
   ),
   notes_api = list(
-    base_url = "https://script.google.com/macros/s/AKfycby8_RuLj5hKxi129ru32cpEojVimffD2msCSl-I9r9a1LfZe9Ht-yLPbiDHVatm48g/exec",
-    token = "OSUbaseball"
+    base_url = "https://script.google.com/macros/s/AKfycbyXJWWRgPib1sSgMtuYCjRW6Wa7UCiC2ojABZHbXSkrhQGogOOYNSMWp4vpNwRYJDdqZg/exec",
+    token = "Harvardbaseball"
   ),
   extra = list(
-    school_name = "Oklahoma State",
+    school_name = "Harvard",
     ftp_folder = "trackman",
     cloudinary_folder = "trackman"
   )
