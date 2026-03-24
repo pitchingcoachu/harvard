57e2017ae85f4c156c54fb4f67c041b1e244e764
pitchingcoachu <jgaynor@pitchingcoachu.com>
2026-03-23 22:09:13 -0700

    Change default TEAM_CODE from LSU to Harvard

--- scripts/rebuild_video_map_from_cloudinary.R
+++ scripts/rebuild_video_map_from_cloudinary.R
@@ -14,8 +14,8 @@ source("pitch_data_service.R")
 
 `%||%` <- function(a, b) if (is.null(a) || length(a) == 0 || all(is.na(a))) b else a
 
-team_code <- toupper(trimws(Sys.getenv("TEAM_CODE", "LSU")))
-if (!nzchar(team_code)) team_code <- "LSU"
+team_code <- toupper(trimws(Sys.getenv("TEAM_CODE", "Harvard")))
+if (!nzchar(team_code)) team_code <- "Harvard"
 
 cloud_name <- Sys.getenv("CLOUDINARY_CLOUD_NAME", "")
 api_key <- Sys.getenv("CLOUDINARY_API_KEY", "")
